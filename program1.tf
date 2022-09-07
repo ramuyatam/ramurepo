992c88a8b2b78ed59d308be919224f23504fa606
ramuyatam <107263064+ramuyatam@users.noreply.github.com>
2022-09-07 17:06:37 +0530

    Update program1.tf

--- program1.tf
+++ program1.tf
@@ -1,16 +1,16 @@
 provider "aws" {
 region "us-east-2"
-access_key = ""
-secret_key = ""
+access_key = "AKIA3CED4V6MKPEOSKSV"
+secret_key = "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 "
 resource "aws_instance" "instance1" {
 
-ami = "ami-0ba62214afa52bec7"
+ami = ""ami-0568773882d492fc8
 
 instance_type = "t2.micro"
 
 tags = {
 
-Name = "RHEL-8"
+Name = "ramu1234"
 
 }
 
